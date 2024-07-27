8069fa5e898a40c3226c718ab45e968a022c3953
hoge1e3 <hoge1e3@gmail.com>
2024-07-27 21:00:50 +0000

    [a038 2024-07-27]ini.sh

--- ini.sh
+++ ini.sh
@@ -1,8 +1,8 @@
 cd ~/sync
-sh co 479d2b2c4a72cefd72d0672a3b003bb1cc2c43a67f9ac7eaceaccb86be86a6d1
+sh co bd74596ebd68f7738b8b3074e4794404bdc1dee6ec83930e7da5b17b6574865d
 
 
-cd ~/node_modles/@hoge1e3/nw-fs
+cd ~/node_modules/@hoge1e3/nw-fs
 cp ~/jsmod/tesfs.js test.mjs
 npm run test-node
 
