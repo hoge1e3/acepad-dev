1c8092e0104a9f386db5d44f99e98fe0a0bba00d
hoge1e3 <hoge1e3@gmail.com>
2024-07-29 14:52:36 +0000

    [0186 2024-07-29]ini.sh

--- ini.sh
+++ ini.sh
@@ -1,15 +1,15 @@
 cd ~/sync
-sh co c61d7aa3fc9af52ad376ca658f2147514a3bd49e9dc9d2f0ec1222a50e6526e2
+sh co 2edf8dad7c3f0d171f6ec645ccffdc95aca8b6e1d464b544bd4c6d318478bb3f
 
 #cp nd ~/bin/
 #cp bin/sync sync.js
 #chmod 755 ~/bin/nd
-#cd /tmp/
-#mkdir synctes
-#cd synctes
-#nd ~/jsmod/sync.js clone jsmod|less
+cd /tmp/
+mkdir synctes
+cd synctes
+nd ~/jsmod/sync.js clone jsmod|less
 
-cd ~/node_modules/@hoge1e3/fs-nw
-cp ~/jsmod/tesfs.js test.mjs
-npm run test-node
+#cd ~/node_modules/@hoge1e3/fs-nw
+#cp ~/jsmod/tesfs.js test.mjs
+#npm run test-node
 
