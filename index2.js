abd30eab1a750f046d764649593fd461455ecb63
hoge1e3 <hoge1e3@gmail.com>
2024-08-21 21:37:00 +0000

    [0008 2024-08-21]+.bin/dotenv, +.bin/sync, +acepad-dotenv/acepad-dotenv.js

--- index2.js
+++ index2.js
@@ -3,9 +3,11 @@ import * as ace from "acepad-with-fs";
 export async function main(){
     sh.cd(sh.resolve(import.meta.url).up());
     await initCmds(sh);
+    await sh.dotenv();
     ace.main.call(sh);
 }
 async function initCmds(sh){
     sh.addPath(sh.resolve("bin/").path());
+    sh.addPath(sh.resolve("node_modules/.bin/").path());
 }
 main().then((s)=>0,(e)=>console.error(e.stack));
