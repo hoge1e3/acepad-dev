0c5dfe948fb70e48d1f1fcfded5815a508f4c978
hoge1e3 <hoge1e3@gmail.com>
2024-08-24 21:21:16 +0000

    [8f2f 2024-08-24]+.bin/acepad-polyfiller.js, +.bin/acepad-suggest.js, +fs/fs.js

--- index2.js
+++ index2.js
@@ -2,12 +2,6 @@ import {sh} from "acepad-os-jsm";
 import * as ace from "acepad-with-fs";
 export async function main(){
     sh.cd(sh.resolve(import.meta.url).up());
-    await initCmds(sh);
-    await sh.dotenv();
     ace.main.call(sh);
-}
-async function initCmds(sh){
-    sh.addPath(sh.resolve("bin/").path());
-    sh.addPath(sh.resolve("node_modules/.bin/").path());
 }
 main().then((s)=>0,(e)=>console.error(e.stack));
