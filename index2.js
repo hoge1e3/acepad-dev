076be0156870eba3ebb65421622c8d667a77dcf4
hoge1e3 <hoge1e3@gmail.com>
2024-09-22 16:25:32 +0000

    [ae8e 2024-09-22]+bin/expbin, -acepad-with-fs/acepad-with-fs(3b44eb27).js, -acepad-with-fs/acepad-with-fs(9399e869).js

--- index2.js
+++ index2.js
@@ -2,6 +2,7 @@ import {sh} from "acepad-os-jsm";
 import * as ace from "acepad-with-fs";
 export async function main(){
     sh.cd(sh.resolve(import.meta.url).up());
-    ace.main.call(sh);
+    await ace.main.call(sh);
+    if(sh.hasCmd("filewatch")) sh.filewatch();
 }
 main().then((s)=>0,(e)=>alert(e.stack));
