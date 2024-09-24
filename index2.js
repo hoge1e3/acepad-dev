d370fe6d7e73e6e016291d28b60ebbbd91e3c3d5
hoge1e3 <hoge1e3@gmail.com>
2024-09-24 20:20:12 +0000

    [3ca0 2024-09-24]+logging.js, .bin/acepad-suggest.js, .bin/dotenv

--- index2.js
+++ index2.js
@@ -1,8 +1,20 @@
 import {sh} from "acepad-os-jsm";
 import * as ace from "acepad-with-fs";
+import {add as addLog} from "./logging.js";
+import * as dotenv from "acepad-dotenv";
+import {main as sug} from "acepad-suggest";
 export async function main(){
     sh.cd(sh.resolve(import.meta.url).up());
-    await ace.main.call(sh);
+    sh.addPath(sh.resolve("bin/").path());
+    dotenv.configUp(sh.resolve("./"));
+    let acepad=await ace.main.call(sh);
     if(sh.hasCmd("filewatch")) sh.filewatch();
+    sh["acepad-polyfiller.js"]();
+    acepad.events.on("keyclick",({b})=>{
+        if(acepad.getCurrentEditor().nolog)return ;
+        addLog(sh,{text:b.innerText});
+        //console.log("k",b.innerText);
+    });
+    sug.call(sh);
 }
 main().then((s)=>0,(e)=>alert(e.stack));
