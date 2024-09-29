53b378bc67f1dbfe2034d4c46dc8d3293bf4a626
hoge1e3 <hoge1e3@gmail.com>
2024-09-29 16:43:36 +0000

    [7128 2024-09-29]+button.html, test/test-treeview.js, acepad-keysel/acepad-keysel.js

--- index2.js
+++ index2.js
@@ -5,8 +5,9 @@ import * as dotenv from "acepad-dotenv";
 import {main as sug} from "acepad-suggest";
 export async function main(){
     sh.cd(sh.resolve(import.meta.url).up());
-    sh.addPath(sh.resolve("bin/").path());
     dotenv.configUp(sh.resolve("./"));
+    sh.set("path",sh.getenv("path")||"");
+    //sh.addPath(sh.resolve("bin/").path());
     let acepad=await ace.main.call(sh);
     if(sh.hasCmd("filewatch")) sh.filewatch();
     sh["acepad-polyfiller.js"]();
