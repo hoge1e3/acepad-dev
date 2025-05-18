007732ebd7b5adccd00de1b1d25c3472f7ef03e6
hoge1e3 <hoge1e3@gmail.com>
2025-05-18 15:23:08 +0900

    @hoge1e3/ajax @acepad/bottomup

---
+++ sbin/tmp/2025_0517_095618.js
@@ -0,0 +1,7 @@
+#!run
+
+export async function main(){
+  console.log("kp",
+    document.querySelector("#acepad").querySelector(".keypad")
+  );
+}
---
+++ sbin/tmp/2025_0517_134916.js
@@ -0,0 +1,5 @@
+#!run
+
+export async function main(){
+return 4*1000/60;
+}
