66952629369b50d07acc18dbade24eb1b29b41c7
hoge1e3 <hoge1e3@gmail.com>
2025-06-29 11:15:10 +0900

    0629

---
+++ sbin/build.js
@@ -0,0 +1,101 @@
+#!run
+import * as pNode from "petit-node";
+//import {test}  from "./test.cjs";
+export async function main(){
+  const target="/jsmod/index2.js";
+  const a=pNode.loadedModules();
+  let mod=a.getByPath(target);
+  console.log("pn",a.getByPath("petit-node"));
+  /*;
+  
+  for (let m of a) {
+    if (m.path===target) entry=m;
+      
+  }*/
+  if (!mod) {
+    throw new Error(`module for ${target} not found`);
+  }
+  //console.log(entry);
+  /*for (const d of mod.dependencies) {
+    console.log(d.url, d.path);
+  }*/
+  let mods=[];
+  let m2id=new WeakMap();
+  function getId(mod) {
+    if(m2id.has(mod))return m2id.get(mod);
+    for (const d of mod.dependencies) {
+      getId(d);
+      
+      //console.log(d.url, d.path);
+    }
+    m2id.set(mod,mods.length);
+    mods.push(mod);
+  }
+  getId(mod);
+  const Re="cheediwh"+"sifeudi";
+  const Di="jejf"+"ksjfi";
+  let buf="";
+  const q=(d)=>JSON.stringify(d);
+  for(let m of mods){
+    if(m.type==="Builtin"){
+      buf+=`b(${q(m.path)});\n`;
+    }else{
+      let rep=m.generatedCode;
+      for(let d of m.dependencies){
+        rep=replaceAll(rep,d.url,Re+Di+m2id.get(d)+Re);
+        
+        
+      }
+      rep=rep.replace(/\bimport\s*\.\s*meta\s*\.\s*url\b/g,
+      q(m.path));
+      const sa=rep.split(Re).map((s)=>
+        s.startsWith(Di)?
+          s.substring(Di.length)-0:s);
+      buf+=`es(${q(m.path)},${q(sa)});\n`;
+      console.log(m2id.get(m),m.path, sa);
+    }
+    //console.log(m2id.get(m),m);
+  }
+  this.resolve("/quick.js").text(
+  replaceAll((tmpl+""),"//insert",buf)+
+  "tmpl();"
+  );
+  //(this.resolve("/quick.js").text());
+  //this.echo(buf);
+  //console.log(test(50));
+}
+function replaceAll(s,f, t) {
+  
+    let result = '';
+    let i = 0;
+    while (true) {
+      const j = s.indexOf(f, i);
+      if (j === -1) {
+        result += s.slice(i);
+        break;
+      }
+      result += s.slice(i, j) + t;
+      i = j + f.length;
+    }
+    return result;
+
+}
+function tmpl(){
+let ld=[];
+let a=pNode.loadedModules();
+function es(path,a){
+//             saveAs(new Blob([content], { type: "application/zip" }), dir.name().replace(/[\/\\]$/, "") + ".zip");
+ const g=a.map((s)=>
+  typeof s==="string"?s:ld[s].url
+ ).join("");
+  const b=new Blob([g],{type:"text/javascript"});
+  const url = URL.createObjectURL(b);
+  ld.push({url});
+}
+function b(path){
+  const url=a.getByPath(path).url;
+  ld.push({url});
+}
+//insert
+import(ld.pop().url);
+}
