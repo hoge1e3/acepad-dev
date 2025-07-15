9eb363de25ce088cf8169f2348eb950c56310b6d
hoge1e3 <hoge1e3@gmail.com>
2025-07-15 10:10:00 +0900

    0715

--- sbin/depGraph.js
+++ sbin/depGraph.js
@@ -18,6 +18,7 @@ export async function main(){
   const tr=(m)=>{
     const s=new Set();
     for(let d of m.dependencies){
+      if(!d.path.includes("node_modules/acepad/"))continue;
       for(let dd of tr(d))s.add(dd);
       s.add(d);
     }
@@ -31,7 +32,33 @@ export async function main(){
     forEach(i=>s[i]=(i%10)+"");
     this.echo((i+"").padStart(2,"0"), s.join(""),ns[i].path);
   }
+  const detatch=(p)=>{
+    const detatched=tr(p);
+    detatched.add(p);
+    //console.log("detatched",detatched);
+    const border=new Set();
+    for(let i=0;i<ns.length;i++){
+      if(detatched.has(ns[i]))continue;
+      for(let d of ns[i].dependencies){
+        if(detatched.has(d)){
+          border.add(d);
+          //this.echo(ns[i].path, d.path);
+        }
+      }
+    }
+    return [detatched,border];
+  };
+  for(let i=0;i<ns.length;i++){
+    const p=ns[i];
+    const [d,b]=detatch(p);
+    this.echo(p.path,d.size,b.size);
+  }
   /*console.log([...a].filter(
     ({path})=>path.match(/main.js/)).
     map(({path})=>path));*/
 }
+/*
+入れ物と中身を分ける
+例えば、コマンドを追加する処理の定義と具体的なコマンドは分ける
+設定を管理する部分とデフォルトの設定内容は分ける
+*/
