2fb14d84796c36b7b5338b0c6639f8f9a9342a2f
hoge1e3 <hoge1e3@gmail.com>
2025-08-26 09:48:55 +0900

    show-vconsole if not exists / prevent remote vconsole css on open("ace.html")

--- sbin/tmp/2025_0823_093327.js
+++
@@ -1,8 +0,0 @@
-#!run
-import {x} from "./lib.js";
-export async function main(){
-  this.echo(x);
-  await this.sleep(1);
-  this.echo(x);
-  
-}
---
+++ sbin/tmp/2025_0825_193805.js
@@ -0,0 +1,10 @@
+#!run
+import {initVConsole,showVConsole} from "show-vconsole";
+
+export async function main(){
+  showVConsole();
+  document.body.innerHTML="";
+//  document.head.innerHTML="";
+/*  const i=document.body.innerHTML;
+return i.substring(i.indexOf("__"+"vconsole")-30)*/
+}
