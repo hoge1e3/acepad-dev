0ddff9086ef14b7e5e2e933176635f5838490e05
hoge1e3 <hoge1e3@gmail.com>
2025-09-18 17:02:11 +0900

    input-overlay in iframe

--- sbin/input-m.js
+++ sbin/input-m.js
@@ -1,5 +1,11 @@
-import {activate} from "ace-overlay";
-function main(){
-  activate(document.body,{dynamic:true});
+import {activate} from "@acepad/overlay";
+async function loadAce(){
+  if(globalThis.ace)return ;
+  const url="https://cdn.jsdelivr.net/npm/ace-builds@1.39.0/src-noconflict/ace.js";
+  await import(url);
+}
+async function main(){
+  await loadAce();
+  activate(acepad,document.body,{dynamic:true});
 }
 main();
