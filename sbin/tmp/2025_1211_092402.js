4c3fa1ca45e7b6431187329e45ed8a428a64669d
hoge1e3 <hoge1e3@gmail.com>
2026-01-09 17:05:20 +0900

    misc

--- sbin/tmp/2025_1211_092402.js
+++
@@ -1,8 +0,0 @@
-#!run
-
-export async function main(){        
-  for(let i=0;i<100;i+=7){
-    this.echo(i,i.toString(8))
-  }
-  return ;
-}
--- sbin/tmp/2026_0108_134451.js
+++ sbin/tmp/2026_0108_134451.js
@@ -1,5 +1,5 @@
 #!run
 
 export async function main(){
-  return this.$PNODE_URL;
+  return p;
 }
