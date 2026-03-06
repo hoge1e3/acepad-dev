67adb6dc0cb3d7d54af00e7c447bdbcf22f0dbc5
hoge1e3 <hoge1e3@gmail.com>
2026-03-06 10:19:57 +0900

    list of pNode props?

---
+++ sbin/tmp/2026_0303_211804.js
@@ -0,0 +1,7 @@
+#!run
+["CJSCompiler","CompiledESModule","ESModule","ESModuleCompiler","FS","ModuleEntry","NodeModule","addAlias","addAliases","addPrecompiledCJSModule","addPrecompiledESModule","boot","convertStack","core","createModuleURL","default","events","file","getAliases","getCore","getDeviceManager","getFS","getNodeLikeFs","importModule","init","loadedModules","on","require","resolveEntry","thisUrl","urlToFile","urlToPath","version"]
+
+export async function main(){
+  return JSON.stringify(
+    Object.keys(pNode));
+}
