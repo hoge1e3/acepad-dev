7ace9520c543b2e011063affc5cc3ebcec57852b
hoge1e3 <hoge1e3@gmail.com>
2026-04-13 20:16:32 +0900

    index.js shrink

--- index.js
+++ index.js
@@ -1,87 +1,31 @@
 import {sh} from "@acepad/os-jsm";
 import * as ace from "@acepad/with-fs";
-import {add as addLog} from "./logging.js";
 import * as dotenv from "@acepad/dotenv";
 import * as pNode from "petit-node";
 import {installBootMenu} from "@acepad/installer";
-import {sessionInfo} from "@acepad/sessions";
+//import {sessionInfo} from "@acepad/sessions";
 export async function main(opt={}){
-    sh.cd(sh.resolve(import.meta.url).up());
-    sh.$home=sh.getcwd().path();
-    if (!sh.getcwd().path().startsWith("/tmp")){
-      installBootMenu(sh,"home",sh.getcwd(),{call:["main"]});
-    }
-    dotenv.configUp(sh.resolve("./"));
-    sh.set("path",sh.getenv("path")||"");
-    //const s=await submenu();
-    //console.log("submenu",s);
-    Error.stackTraceLimit = 100;
-    let acepad=await ace.main.call(sh);
-    if (process.env.ACEPAD_DEBUG) {
-        if(sh.hasCmd("filewatch")) sh.filewatch({addLog});
-    }
-    if (process.env.ACEPAD_LOGGING) {
-        acepad.events.on("keyclick",({b})=>{
-            if(acepad.getCurrentEditor().nolog)return ;
-            //console.log(b,b.classList.contains("guide"));
-            addLog(sh,{
-              text:b.innerText,
-              ...(b.classList.contains("guide")?
-                {guide:true}:{})
-            });
-        });
-        acepad.events.on("complete",(e)=>{
-          addLog(sh,{
-            text:e.text,
-            complete:1
-            
-          });
-        });
-        acepad.getMainEditor().
-        on("changeSession", (e)=>{
-          const si=sessionInfo(e.session);
-          addLog(sh,{
-            name:si.name,
-            ...(
-              si.file?
-              {file:si.file+""}:{})
-          });
-        });
-    
-         acepad.getMainEditor().
-      container.addEventListener(
-        "touchstart",()=>{
-          //console.log("touced")
-          addLog(sh,{
-            touched:1
-          });
-        });
-     
-    }
-    //sug.call(sh);
-    setTimeout(()=>
-    console.log("pNode.version",pNode.version),1500);
-    if(opt.cmd){
-      sh.exec(opt.cmd);
-    }
-    if (process.env.GSYNC_RUN_ON_BOOT) {
-      sh.gsync("newer");     
-    }
+  sh.cd(sh.resolve(
+    import.meta.url
+  ).up());
+  sh.$home=sh.getcwd().path();
+  if (!sh.getcwd().path().startsWith("/tmp")){
+    installBootMenu(sh,"home",sh.getcwd(),{call:["main"]});
+  }
+  dotenv.configUp(sh.resolve("./"));
+  sh.set("path",sh.getenv("path")||"");
+  Error.stackTraceLimit = 100;
+  let acepad=await ace.main.call(sh);
+  setTimeout(()=>
+  console.log("pNode.version",pNode.version),1500);
+  if(opt.cmd){
+    sh.exec(opt.cmd);
+  }
     //sh.try("wcart-menu");
 }
-async function submenu(){
-  const b=globalThis.pNodeBootLoader;
-  if (!b) return;
-  const s=b.getSelectedSubmenu();
-  if (!s) return;
-  return await s;
-}
-if(!globalThis.pNodeBootLoader?.version){
+/*if(!globalThis.pNodeBootLoader?.version){
   main().then((s)=>0,(e)=>alert(e.stack));
-}
+}*/
 export function install(){
   main({install:true});
-}
-export function test(x){
-  alert(x);
 }
