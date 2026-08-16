0c900ffee8440bffda46a30f66888dd8bcfe0b06
hoge1e3 <hoge1e3@gmail.com>
2026-08-16 10:49:27 +0900

    mode-tonyu

--- samples/tonyu/Actest/js/concat.js
+++ samples/tonyu/Actest/js/concat.js
@@ -18,15 +18,22 @@ Tonyu.klass.define({
         _this.fillStyle="white";
         _this.radius=20;
         Tonyu.globals.$Screen.setBGColor("skyblue");
+        _this.c=- 100;
         while (_this.x<2000) {
           Tonyu.checkLoop();
+          _this.c+=20;
           if (_this.getkey("right")) {
             _this.x+=2;
           }
           _this.y+=Tonyu.globals.$touches[0].vy;
           _this.radius=16+_this.cos(Tonyu.globals.$frameCount*10)*3;
-          if (Tonyu.globals.$frameCount%8==0) {
+          if (_this.c>_this.y) {
+            _this.c=0;
             new Tonyu.classes.user.Bullet({x: _this.x,y: _this.y});
+            
+          }
+          if (Tonyu.globals.$frameCount%18==0) {
+            new Tonyu.classes.user.Enemy({x: Tonyu.globals.$screenWidth,y: _this.rnd(Tonyu.globals.$screenHeight)});
             
           }
           _this.update();
@@ -40,15 +47,22 @@ Tonyu.klass.define({
         _this.fillStyle="white";
         _this.radius=20;
         Tonyu.globals.$Screen.setBGColor("skyblue");
+        _this.c=- 100;
         while (_this.x<2000) {
           yield null;
+          _this.c+=20;
           if (_this.getkey("right")) {
             _this.x+=2;
           }
           _this.y+=Tonyu.globals.$touches[0].vy;
           _this.radius=16+_this.cos(Tonyu.globals.$frameCount*10)*3;
-          if (Tonyu.globals.$frameCount%8==0) {
+          if (_this.c>_this.y) {
+            _this.c=0;
             new Tonyu.classes.user.Bullet({x: _this.x,y: _this.y});
+            
+          }
+          if (Tonyu.globals.$frameCount%18==0) {
+            new Tonyu.classes.user.Enemy({x: Tonyu.globals.$screenWidth,y: _this.rnd(Tonyu.globals.$screenHeight)});
             
           }
           (yield* _this.fiber$update(_thread));
@@ -59,7 +73,7 @@ Tonyu.klass.define({
       __dummy: false
     };
   },
-  decls: {"methods":{"main":{"nowait":false,"isMain":true,"vtype":{"params":[],"returnValue":null}}},"fields":{}}
+  decls: {"methods":{"main":{"nowait":false,"isMain":true,"vtype":{"params":[],"returnValue":null}}},"fields":{"c":{}}}
 });
 Tonyu.klass.define({
   fullName: 'user.Bullet',
@@ -76,7 +90,7 @@ Tonyu.klass.define({
         _this.fillStyle="red";
         while (true) {
           Tonyu.checkLoop();
-          _this.x+=5;
+          _this.x+=10;
           if (_this.screenOut()) {
             _this.die();
           }
@@ -91,7 +105,7 @@ Tonyu.klass.define({
         _this.fillStyle="red";
         while (true) {
           yield null;
-          _this.x+=5;
+          _this.x+=10;
           if (_this.screenOut()) {
             _this.die();
           }
@@ -104,6 +118,78 @@ Tonyu.klass.define({
     };
   },
   decls: {"methods":{"main":{"nowait":false,"isMain":true,"vtype":{"params":[],"returnValue":null}}},"fields":{}}
+});
+Tonyu.klass.define({
+  fullName: 'user.Enemy',
+  shortName: 'Enemy',
+  namespace: 'user',
+  superclass: Tonyu.classes.kernel.Actor,
+  includes: [],
+  methods: function (__superClass) {
+    return {
+      main :function _trc_Enemy_main() {
+        var _this=this;
+        
+        _this.width=_this.height=25;
+        _this.fillStyle="black";
+        _this.vy=_this.rndFloat(- 10,10);
+        while (true) {
+          Tonyu.checkLoop();
+          _this.x-=3;
+          if (_this.screenOut()) {
+            _this.die();
+          }
+          _this.b=_this.crashTo(Tonyu.classes.user.Bullet);
+          if (_this.b) {
+            _this.b.die();
+            _this.die();
+            
+          }
+          _this.vy+=0.5;
+          _this.y+=_this.vy;
+          _this.c=_this.clamped(_this.y,0,Tonyu.globals.$screenHeight);
+          _this.y+=_this.c;
+          if (_this.c) {
+            _this.vy*=- 1;
+          }
+          _this.update();
+          
+        }
+      },
+      fiber$main :function* _trc_Enemy_f_main(_thread) {
+        var _this=this;
+        
+        _this.width=_this.height=25;
+        _this.fillStyle="black";
+        _this.vy=_this.rndFloat(- 10,10);
+        while (true) {
+          yield null;
+          _this.x-=3;
+          if (_this.screenOut()) {
+            _this.die();
+          }
+          _this.b=_this.crashTo(Tonyu.classes.user.Bullet);
+          if (_this.b) {
+            _this.b.die();
+            _this.die();
+            
+          }
+          _this.vy+=0.5;
+          _this.y+=_this.vy;
+          _this.c=_this.clamped(_this.y,0,Tonyu.globals.$screenHeight);
+          _this.y+=_this.c;
+          if (_this.c) {
+            _this.vy*=- 1;
+          }
+          (yield* _this.fiber$update(_thread));
+          
+        }
+        
+      },
+      __dummy: false
+    };
+  },
+  decls: {"methods":{"main":{"nowait":false,"isMain":true,"vtype":{"params":[],"returnValue":null}}},"fields":{"vy":{},"b":{},"c":{}}}
 });
 
 });
