2042f8bc1f9b841a7b6a57ef9410b80728d09912
hoge1e3 <hoge1e3@gmail.com>
2026-09-13 10:04:38 +0900

    tonyu-ide

---
+++ sbin/tmp/2026_0909_210604.js
@@ -0,0 +1,7 @@
+#!run
+import pNode from "petit-node";
+export async function main(){
+  const FS=pNode.getFS();
+  return this.resolve("2026_0909_210604.js").dataURL()
+  return  FS.isFile;FS.SFile
+}
